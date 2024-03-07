9208f6e59fb8a5ea43d454fc369ef89ff271c34d
supawit nipuna <supawit7164@gmail.com>
2024-03-07 14:27:06 +0700

    ...

--- pages/index.vue
+++ pages/index.vue
@@ -22,39 +22,46 @@
     </v-container>
     <v-container>
       <h1 class="ml-5">รายวิชาที่กำลังเปิดให้ลงทะเบียน</h1>
-      <v-row>
-        <v-col v-for="n in 6" :key="n" class="d-flex child-flex" cols="4">
-          <v-img
-            :src="require('~/assets/education.png')"
-            aspect-ratio="1"
-            class="grey lighten-2"
-          >
-            <template v-slot:placeholder>
-              <v-row class="fill-height ma-0" align="center" justify="center">
-                <v-progress-circular
-                  indeterminate
-                  color="grey lighten-5"
-                ></v-progress-circular>
-              </v-row>
-            </template>
-          </v-img>
-        </v-col>
-      </v-row>
+<v-carousel v-model="model">
+    <v-carousel-item
+      v-for="(color, i) in colors"
+      :key="color"
+    >
+      <v-sheet
+        :color="color"
+        height="100%"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="text-h2">
+            Slide {{ i + 1 }}
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: "",
-  data() {
-    return {
-      // ข้อมูลเกี่ยวกับเรา
-    };
-  },
-};
+  export default {
+    data: () => ({
+      model: 0,
+      colors: [
+        'primary',
+        'secondary',
+        'yellow darken-2',
+        'red',
+        'orange',
+      ],
+    }),
+  }
 </script>
-
 <style scoped>
 
 </style>
