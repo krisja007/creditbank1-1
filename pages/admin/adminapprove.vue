bbc84176008e6a0eeb7e18f144309ae3f12a2080
VeerachaiPTee <devtee.pp@gmail.com>
2024-05-01 00:01:56 +0700

    Fix admin button

--- pages/admin/adminapprove.vue
+++ pages/admin/adminapprove.vue
@@ -1,82 +1,48 @@
 <template>
-      <div class="">
+  <div class="">
     <NavbarAd />
     <v-card depressed color="light-green lighten-4 ">
-    <v-row>
- <v-col class="">
-      <v-btn class="headline pa-7" width="100%" elevation="5" @click="goadminpage">
-        <v-icon large>mdi mdi-home</v-icon>กลับสู่หน้าหลักแอดมิน
-      </v-btn>
-    </v-col>
-    
-<v-col cols="12">
-<h2 class="display-1 text-center">รายชื่อผู้ขอเปิดสิทธิ์สร้างโพส</h2>      
-</v-col>
-<v-container>
-<v-card class="py-10"
+      <v-row>
+        <v-col class="">
+          <v-btn
+            class="headline pa-7"
             width="100%"
-            elevation="8"
-            rounded="xl"
-            v-for="(item, admin) in approve"
-            :key="admin"
->                         
-              <v-row>
-                <v-col cols="6">
-                  <p class="title tX-color">ชื่อ: {{ item.name }}</p></v-col
-                >
-                <v-col cols="7">
-                  <p class="title mr-10">สถานะ: {{ item.role }}</p>
-                  </v-col>
-                <v-col cols="10"
-                  ><p class="title tX-color">สถานที่เรียน: {{ item.location }}</p>
-                </v-col>
-             <v-col cols="4" md="4" lg="4" class="d-flex justify-start align-center pl-5 pt-2">
-          <v-btn class="" depressed color="success" @click="goDetail"
-            >ยืนยัน</v-btn
+            elevation="5"
+            @click="goadminpage"
           >
+            <v-icon large>mdi mdi-home</v-icon>กลับสู่หน้าหลักแอดมิน
+          </v-btn>
         </v-col>
-             <v-col cols="4" md="4" lg="4" class="d-flex justify-start align-center pl-5 pt-2">
-          <v-btn class="" depressed color="red darken-2" @click="goDetail"
-            >ปฏิเสธ</v-btn
-          >
-        </v-col>
-        
-        </v-row>          
-</v-card>
-<v-card class="py-10"
-            width="100%"
-            elevation="8"
-            rounded="xl"
-            v-for="(item, admin) in approve"
-            :key="admin"
->                         
-              <v-row>
-                <v-col cols="6">
-                  <p class="title tX-color">ชื่อ: {{ item.name }}</p></v-col
-                >
-                <v-col cols="7">
-                  <p class="title mr-10">สถานะ: {{ item.role }}</p>
-                  </v-col>
-                <v-col cols="10"
-                  ><p class="title tX-color">สถานที่เรียน: {{ item.location }}</p>
-                </v-col>
-             <v-col cols="4" md="4" lg="4" class="d-flex justify-start align-center pl-5 pt-2">
-          <v-btn class="" depressed color="success" @click="goDetail"
-            >ยืนยัน</v-btn
-          >
-        </v-col>
-             <v-col cols="4" md="4" lg="4" class="d-flex justify-start align-center pl-5 pt-2">
-          <v-btn class="" depressed color="red darken-2" @click="goDetail"
-            >ปฏิเสธ</v-btn
-          >
-        </v-col>
-        
-        </v-row>          
-</v-card>
-</v-container>
 
-    </v-row> 
-   
+        <v-col cols="12">
+          <h2 class="display-1 text-center">รายชื่อผู้ขอเปิดสิทธิ์สร้างโพส</h2>
+        </v-col>
+
+        <v-card
+          class="py-10 mx-7 mb-5"
+          width="100%"
+          elevation="8"
+          rounded="xl"
+          v-for="(item, admin) in approve"
+          :key="admin"
+        >
+          <v-row>
+            <v-col cols="6" class="pl-10">
+              <p class="title tX-color">ชื่อ: {{ item.name }}</p>
+              <p class="title mr-10">สถานะ: {{ item.role }}</p>
+              <p class="title tX-color">สถานที่เรียน: {{ item.location }}</p>
+            </v-col>
+            <v-col cols="6" class="d-flex align-center justify-center">
+              <v-btn class="mr-5" depressed color="success" @click="goDetail"
+                >ยืนยัน</v-btn
+              >
+              <v-btn class="ml-5" depressed color="red darken-2" @click="goDetail"
+                >ปฏิเสธ</v-btn
+              >
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-row>
     </v-card>
   </div>
 </template>
@@ -94,10 +60,8 @@ export default {
           name: " อาจารย์แสนดี บุญรอด ",
           role: "อาจารย์",
           location: "มหาวิทยาลัยแม่โจ้ เชียงใหม่ ",
-
-
         },
-      ]
+      ],
     };
   },
   created() {
@@ -107,10 +71,10 @@ export default {
     ...mapMutations({
       SET_LOGIN: "users/SET_LOGIN",
     }),
-     goadminpage() {
+    goadminpage() {
       this.$router.push("/admin/adminpage");
       this.citizenID = "";
-    }, 
+    },
   },
 };
 </script>
